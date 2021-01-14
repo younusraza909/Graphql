866768e085c1a38e43936ad63e67e01648e4daeb
younusraza909 <younusraza909@gmail.com>
2021-01-13 19:15:47 -0800

    Making my own graphql server API,making my first graphQl Query

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,34 @@
-console.log("Hello GraphQl");
+import { GraphQLServer } from "graphql-yoga";
+
+//Type Defination
+const typeDefs = `
+  type Query{
+      hello: String
+      location: String
+      bio: String
+  }
+`;
+
+//Resolver
+const resolvers = {
+  Query: {
+    hello() {
+      return "This is my first query for graphql";
+    },
+    location() {
+      return "Karachi ,Pakistan";
+    },
+    bio() {
+      return "Keen to learn programming. Age 20";
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+});
+
+server.start(() => {
+  console.log("The server is up running");
+});
