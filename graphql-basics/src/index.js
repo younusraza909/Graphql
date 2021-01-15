b2579aa57b12b7d0e0a35e68e27c923360cddf7e
younusraza909 <younusraza909@gmail.com>
2021-01-14 20:27:08 -0800

    working with arrays in graphql

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -2,11 +2,55 @@ import { GraphQLServer } from "graphql-yoga";
 
 //Scalar Type- String,Boolean,Int,Float,ID
 
+// Demo User Data
+const users = [
+  {
+    id: "1",
+    name: "Mike",
+    email: "mike@example.com",
+    age: 20,
+  },
+  {
+    id: "2",
+    name: "sara",
+    email: "Sara@example.com",
+  },
+  {
+    id: "3",
+    name: "andrew",
+    email: "andrew@example.com",
+  },
+];
+
+const posts = [
+  {
+    id: "1",
+    title: "First Post Title",
+    body: "First Post Body",
+    published: true,
+  },
+  {
+    id: "2",
+    title: "Second Post Title",
+    body: "Second Post Body",
+    published: true,
+  },
+  {
+    id: "3",
+    title: "Third Post Title",
+    body: "Third Post Body",
+    published: false,
+  },
+];
+
 //Type Defination
 const typeDefs = `
   type Query{
+     users(query: String): [User!]!
+     posts(query: String):[Post!]!
      me: User!
      post: Post!
+ 
   }
 
   type User{
@@ -27,6 +71,24 @@ const typeDefs = `
 //Resolver
 const resolvers = {
   Query: {
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return users;
+      }
+
+      return users.filter((user) => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase());
+      });
+    },
+    posts(parent, args, ctx, info) {
+      if (!args.query) {
+        return posts;
+      }
+
+      return posts.filter((post) => {
+        return post.title.toLowerCase().includes(args.query.toLowerCase());
+      });
+    },
     me() {
       return {
         id: "123098",
